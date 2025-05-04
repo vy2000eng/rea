a5ac06ad011067f4004622d9f2a4aff2bdd05e1d
vladyslavyatsuta <vy2000eng@gmail.com>
2025-05-04 02:19:33 -0400

    updted build again

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,17 +1,8 @@
 import { AppSidebar } from "@/components/app-sidebar"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
+
 import {
   SidebarInset,
   SidebarProvider,
-  SidebarTrigger,
 } from "@/components/ui/sidebar"
 import { Index } from "@/pages/Index"
 
